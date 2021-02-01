431e59a96b7b92cea8d9faca900e44d8d713f4a8
andrewsweeney <andrew.sweeney@turner.com>
2021-02-01 16:21:27 -0500

    Committed for SmashMouth

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Thank you.
-        </p>
+        Making a change and seeing it change for change. Because we could all use a little...<br/>
+        Chaaaaaange.
         <a
           className="App-link"
           href="https://reactjs.org"
